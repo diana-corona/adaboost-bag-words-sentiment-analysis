22fa5309370dc1d2486edbdaea492247af22eb99
Diana Corona <diana.corona.g@gmail.com>
2021-09-13 12:35:03 +0100

    add: save prediction

--- predict.py
+++ predict.py
@@ -1,13 +1,22 @@
-
 from modules.BoostingClassifier import BoostingClassifier
 from modules.BagOfWords import extract_bag_of_words
 from sklearn.metrics import accuracy_score
+import numpy as np
+
+def save(config,X_test,y_pred):
+    sentiment_dict = {'1':'positive','0':'negative'}
+    y_results = [sentiment_dict[str(int(prediction))] for prediction in y_pred]
+    np.savetxt(config['results_file_path'], y_results, header='sentiment_prediction', fmt='%s')
+    
+
 
 def predict(config,boostingClassifier):
     (X_test, Y_test) = extract_bag_of_words(config['test_file_path'],config)
     bc = boostingClassifier
     y_pred = bc.predict(X_test)    
     print('accuracy', accuracy_score(Y_test, y_pred))
+    if 'results_file_path' in config:
+        save(config,X_test,y_pred)
     return y_pred
 
 def train_and_predict(config):
@@ -17,4 +26,7 @@ def train_and_predict(config):
     bc.fit(X_train, Y_train)
     y_pred = bc.predict(X_test)    
     print('accuracy', accuracy_score(Y_test, y_pred))
+    sentiment_dict = {1:'positive',0:'negative'}
+    if 'results_file_path' in config:
+        save(config,X_test,y_pred)
     return y_pred
